9df52c9e67a171dd9c6e6de31ac0b46b638d9a39
Donato Doley <donatodoley1@gmail.com>
2020-10-30 19:21:30 +0530

    additions

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const container = document.querySelector('#container');
+
 function makeGrid(rows, cols){
     container.style.setProperty('--grid-cols', cols);
     container.style.setProperty('--grid-rows', rows);
-    
+    for(let i =0 ; i<(rows*cols);i++){
+        let cell = document.createElement('div');
+        cell.innerText = i + 1;
+        container.appendChild(cell).className = "grid-item";
+    }
 }
+makeGrid(16,16);
