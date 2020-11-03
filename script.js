569f5f54f2a8596c25476f5360e93d60dcc0b011
Donato Doley <donatodoley1@gmail.com>
2020-11-03 13:44:40 +0530

    added a 16x16 grid using css grid

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function makeGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     for(let i =0 ; i<(rows*cols);i++){
         let cell = document.createElement('div');
-        cell.innerText = i + 1;
         container.appendChild(cell).className = "grid-item";
     }
 }
